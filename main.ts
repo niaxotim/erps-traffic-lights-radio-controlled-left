0c57113caa15294951d0d142ac617f7ab4e8488b
Tim <58301440+niaxotim@users.noreply.github.com>
2023-01-23 19:15:43 +0000

    Added blocks

--- main.ts
+++ main.ts
@@ -1,3 +1,25 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("Start Sequence")
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "Start Sequence") {
+        Start_Lights = true
+    }
+})
+let Start_Lights: Boolean
+radio.setGroup(1)
+Kitronik_STOPbit.trafficLightState(Kitronik_STOPbit.LightStates.Stop)
 basic.forever(function () {
-	
+    if (Start_Lights == true) {
+        basic.pause(2000)
+        Kitronik_STOPbit.trafficLightState(Kitronik_STOPbit.LightStates.GetReady)
+        basic.pause(500)
+        Kitronik_STOPbit.trafficLightState(Kitronik_STOPbit.LightStates.Go)
+        basic.pause(2000)
+        Kitronik_STOPbit.trafficLightState(Kitronik_STOPbit.LightStates.ReadyToStop)
+        basic.pause(1000)
+        Kitronik_STOPbit.trafficLightState(Kitronik_STOPbit.LightStates.Stop)
+        Start_Lights = false
+        radio.sendString("Start Sequence")
+    }
 })
